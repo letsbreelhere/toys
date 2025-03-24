6882df35198166f5db6a46921dd14ec91b2cf1be
letsbreelhere <letsbreelhere@gmail.com>
2025-03-24 11:16:22 -0700

    More-working tromp diagrams

--- tromp/main.py
+++ tromp/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 def lam(var, expr):
   return { "type": "lambda", "var": var, "expr": expr }
@@ -9,7 +10,7 @@ def app(expr1, expr2):
 def var(name):
   return { "type": "var", "name": name }
 
-DIAGRAM_GAP = 50
+DIAGRAM_GAP = 25
 MARGIN = 20
 
 """
@@ -23,24 +24,48 @@ For both lambdas and applications, the size of the diagram is the size of the bo
 We must track free variables in the diagram specially; once a lambda abstraction is introduce, we add a line to connect the lambda abstraction to the free variable.
 """
 
+class BBox:
+  @staticmethod
+  def from_points(top_left, bottom_right):
+    return BBox(top_left[0], top_left[1], bottom_right[0] - top_left[0], bottom_right[1] - top_left[1])
+
+  def __init__(self, x, y, width, height):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+  def top_left(self):
+    return (self.x, self.y)
+
+  def bottom_right(self):
+    return (self.x + self.width, self.y + self.height)
+
+
 def draw_tromp(expr, origin = (0, 0), lambda_heights = {}):
   match expr:
     case { "type": "lambda", "var": var, "expr": expr }:
       lambda_heights[var] = origin[1]
       (bbox, lines) = draw_tromp(expr, (origin[0], origin[1] + DIAGRAM_GAP), lambda_heights)
-      lines.append((origin[0], origin[1], origin[0] + bbox[2] + DIAGRAM_GAP, origin[1]))
-      return ((origin[0], origin[1], bbox[2], bbox[3]), lines)
+      lines.append((origin[0], origin[1], origin[0] + bbox.width, origin[1]))
+      return (BBox(origin[0], origin[1], bbox.width, bbox.height), lines)
 
     case { "type": "app", "expr1": expr1, "expr2": expr2 }:
       (bbox1, lines1) = draw_tromp(expr1, origin, lambda_heights)
-      (bbox2, lines2) = draw_tromp(expr2, (bbox1[0] + bbox1[2] + DIAGRAM_GAP, origin[1]), lambda_heights)
+      (bbox2, lines2) = draw_tromp(expr2, (bbox1.x + bbox1.width + DIAGRAM_GAP, origin[1]), lambda_heights)
+      print(f"in bboxes:")
+      print(f"\tL: {bbox1.x}, {bbox1.y}, {bbox1.width}, {bbox1.height}")
+      print(f"\tR: {bbox2.x}, {bbox2.y}, {bbox2.width}, {bbox2.height}")
       lines = lines1 + lines2
       # Add line connecting bottom lefts of the two boxes
-      print(f"Connecting {bbox1} to {bbox2}")
-      lines.append((bbox1[0], bbox1[1], bbox2[0], bbox1[1]))
+      lines.append((bbox1.x, bbox1.y + bbox1.height, bbox2.x, bbox2.y + bbox2.height))
+      print(f"connected {expr}: {bbox1.x}, {bbox1.y} -> {bbox2.x}, {bbox2.y}")
       # Add a small output line from bottom left down GAP pixels
-      lines.append((bbox1[0], bbox1[3], bbox1[0], bbox1[3] + DIAGRAM_GAP))
-      bbox = (bbox1[0], bbox1[1], bbox2[2], max(bbox1[3], bbox2[3]) + DIAGRAM_GAP)
+      lines.append((bbox1.x, bbox1.y + bbox1.height, bbox1.x, bbox1.y + bbox1.height + DIAGRAM_GAP))
+      top_left = (bbox1.x, bbox1.y)
+      bottom_right = (bbox2.x + bbox2.width + DIAGRAM_GAP, bbox1.y + bbox1.height + DIAGRAM_GAP)
+      bbox = BBox.from_points(top_left, bottom_right)
+      print(f"out bbox: {bbox.x}, {bbox.y}, {bbox.width}, {bbox.height}")
       return (bbox, lines)
 
     case { "type": "var", "name": name }:
@@ -52,7 +77,7 @@ def draw_tromp(expr, origin = (0, 0), lambda_heights = {}):
       lines = [(ox, lambda_line, ox, origin[1])]
       # As with app, add a small output line from bottom left down GAP pixels
       lines.append((ox, origin[1], ox, origin[1] + DIAGRAM_GAP))
-      return ((ox, origin[1] + DIAGRAM_GAP, ox, origin[1] + DIAGRAM_GAP), lines)
+      return (BBox(ox, origin[1] + DIAGRAM_GAP, 0, 0), lines)
 
 if __name__ == "__main__":
   # S combinator λx.λy.λz.x z (y z)
@@ -62,13 +87,9 @@ if __name__ == "__main__":
 
   omega = lam("x", app(var("x"), var("x")))
 
-  test_expr = lam("f", lam("x", app(app(var("f"), var("f")), var("x"))))
+  test_expr = lam("x", lam("y", lam("z", app(var("x"), var("z")))))
   # Get diagram lines and size
   (bbox, lines) = draw_tromp(test_expr)
-
-  print(bbox)
-  print(lines)
-
 
   pygame.init()
   width = 640
@@ -79,15 +100,18 @@ if __name__ == "__main__":
 
   screen.fill((255, 255, 255))
 
-  # Draw all lines
-  for x1, y1, x2, y2 in lines:
-    # Scale and translate coordinates
-    pygame.draw.line(screen, (0, 0, 0), (x1 + MARGIN, y1 + MARGIN), (x2 + MARGIN, y2 + MARGIN), 2)
-
+  line_index = 0
   while running:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
+
+    if line_index < len(lines):
+      x1, y1, x2, y2 = lines[line_index]
+      pygame.draw.line(screen, (0,0,0), (x1 + MARGIN, y1 + MARGIN), (x2 + MARGIN, y2 + MARGIN), 2)
+      pygame.display.flip()
+      pygame.time.wait(100)
+      line_index += 1
 
     pygame.display.flip()
 
